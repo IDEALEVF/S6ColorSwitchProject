9e5131b0a4e8c1b4d46f8c1fefe851bff22f7cc4
Renoy <adrien.pitrou@gmail.com>
2018-02-12 15:18:20 +0100

    penible

--- S6ColorSwitchProject/S6ColorSwitchProject/src/tests/testQuickSort.java
+++ S6ColorSwitchProject/S6ColorSwitchProject/src/tests/testQuickSort.java
@@ -4,8 +4,9 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.Vector;
 
-//import org.junit.Test;
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
+
 
 import main.model.QuickSort;
 import main.model.Pair;
@@ -23,10 +24,12 @@ class testQuickSort{
 		tab.add(new Pair<String, Integer>("Bob", 4));
 		tab.add(new Pair<String, Integer>("Lyse", 1));
 		tab.add(new Pair<String, Integer>("Manon", 9));
-		
-		
+
+
+
 		afficher(tab);
-		
+
+
 		tab2.add(new Pair<String, Integer>("Lyse", 1));
 		tab2.add(new Pair<String, Integer>("Bob", 3));
 		tab2.add(new Pair<String, Integer>("Bob", 4));
@@ -34,18 +37,20 @@ class testQuickSort{
 		tab2.add(new Pair<String, Integer>("Manon", 9));
 		tab2.add(new Pair<String, Integer>("Bob", 16));
 		tab2.add(new Pair<String, Integer>("Bob", 56));
-		
+
+
 		afficher(tab2);
-		
-		
+
+
 		QuickSort q = new QuickSort();
 		q.quickSort(tab,  0,  tab.size()-1);
-		
+
 		afficher(tab);
-		
+
 		assertTrue("Tri incorrect : "+tab, iddentiques(tab,tab2));
 	}
-	
+
+
 	public boolean iddentiques(Vector<Pair<String, Integer>> tab,Vector<Pair<String, Integer>> tab2 ) {
 		for(int i=0;i<tab.size();i++) {
 			if(tab.get(i).second() != tab2.get(i).second()) {
@@ -54,7 +59,7 @@ class testQuickSort{
 		}
 		return true;
 	}
-	
+
 	public void afficher(Vector<Pair<String, Integer>> tab) {
 		for(int i=0; i <tab.size();i++) {
 			System.out.println(tab.get(i));
